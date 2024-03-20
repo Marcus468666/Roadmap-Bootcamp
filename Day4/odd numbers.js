2d2959c90c55a5e56309de3531ad19874dfe0b2a
Marcus Mathuram <marcus.mathuram7@gmail.com>
2024-03-21 01:09:16 +0530

    Done

--- Day4/odd numbers.js
+++ Day4/odd numbers.js
@@ -1,31 +1,31 @@
 //Anonymous function
-let printOddNumbers = function(arr) {
-    for (let num of arr) {
-      if (num % 2 !== 0) {
-        console.log(num);
-      }
+let printOddNumbers = function (arr) {
+  for (let num of arr) {
+    if (num % 2 !== 0) {
+      console.log(num);
     }
-  };
+  }
+};
 
-  printOddNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-  
+printOddNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
 //IIFE
-(function(arr) {
-    for (let num of arr) {
-      if (num % 2 !== 0) {
-        console.log(num);
-      }
+(function (arr) {
+  for (let num of arr) {
+    if (num % 2 !== 0) {
+      console.log(num);
     }
-  })([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  }
+})([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
 //Arrow Function
 
 let printOddNumbers1 = (arr) => {
-    for(let num of arr) {
-        if(num % 2 !==0){
-            console.log(num);
-        }
+  for (let num of arr) {
+    if (num % 2 !== 0) {
+      console.log(num);
     }
+  }
 };
 
 printOddNumbers1([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
